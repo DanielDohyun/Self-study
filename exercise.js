6187c09f7cb5e8c07df9dd08b13cc7e2c371efd5
DanielKimm <66330126+DanielKimm@users.noreply.github.com>
2020-10-07 14:29:52 -0400

    exercise 1

---
+++ exercise.js
@@ -0,0 +1,17 @@
+// if(jQuery) {
+//   alert("jQuery loaded")
+// } else {
+//   alert("no jQuery")
+// }
+
+// select all divs and give them purple background
+$("div").css("background", "purple");
+
+// select divs with class highlight and make them 200px wide 
+$(".highlight").css("width", "200px");
+
+// give orange border to div with id "third"
+$("#third").css("border", "1px solid orange");
+
+// select 1st div and change its font color to pink 
+$("div:first-child").css("color", "pink");
