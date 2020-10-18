e2c16e96cbd1fba654305d971c86c8c9a66ff7f3
DanielKimm <66330126+DanielKimm@users.noreply.github.com>
2020-10-18 17:41:47 -0400

    header styling

--- youtube-clone/src/components/Header/Header.js
+++ youtube-clone/src/components/Header/Header.js
@@ -17,15 +17,15 @@ function Header() {
         <img className="header__logo" src="https://upload.wikimedia.org/wikipedia/commons/b/b8/YouTube_Logo_2017.svg" />
       </div>
     
-      <div className="header__mid">
-        <input type="text" />
-        <SearchIcon />
+      <div className="header__input">
+        <input placeholder="Search" type="text" />
+        <SearchIcon className="header__inputBtn" />
       </div>
       
-      <div className="header__right">
-        <VideoCallIcon />
-        <AppsIcon />
-        <NotificationsIcon />
+      <div className="header__icons">
+        <VideoCallIcon className="header__icon" />
+        <AppsIcon className="header__icon" />
+        <NotificationsIcon className="header__icon" />
         <Avatar 
           alt="Dog"
           src="https://s3.ap-northeast-2.amazonaws.com/elasticbeanstalk-ap-northeast-2-176213403491/media/magazine_img/magazine_311/3-2-%EC%8D%B8%EB%84%A4%EC%9D%BC.jpg"
