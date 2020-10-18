1749f183b20ac1cad4040d082ec69eafec6eb4ed
DanielKimm <66330126+DanielKimm@users.noreply.github.com>
2020-10-18 16:17:51 -0400

    create youtube-clone app

---
+++ youtube-clone/src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDb2p_a2jU9mN_iUFAtjIt_oSDENdJFjoA",
+  authDomain: "clone-247d7.firebaseapp.com",
+  databaseURL: "https://clone-247d7.firebaseio.com",
+  projectId: "clone-247d7",
+  storageBucket: "clone-247d7.appspot.com",
+  messagingSenderId: "89843711544",
+  appId: "1:89843711544:web:4ef0e305f0e226f91e70cd",
+  measurementId: "G-W9XXK79EKE"
+};
