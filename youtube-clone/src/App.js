3ef66eca79b420d71a7718af7937565738de413d
DanielKimm <66330126+DanielKimm@users.noreply.github.com>
2020-10-18 17:58:52 -0400

    changed css to scss + start sidebar comp

--- youtube-clone/src/App.js
+++ youtube-clone/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
+import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
   return (
     <div className="app">
       <Header />
-      
+
+      <div className="app__page">
+        <Sidebar />
+        {/* <RecommendedVideos /> */}
+      </div>
+
     </div>
   );
 }
