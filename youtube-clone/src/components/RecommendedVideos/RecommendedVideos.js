588d8ac86994de48e2622ae2ada93002a404373d
DanielDohyun <kkdh1109@gmail.com>
2020-10-20 16:36:31 -0400

    start videoCard comp

--- youtube-clone/src/components/RecommendedVideos/RecommendedVideos.js
+++ youtube-clone/src/components/RecommendedVideos/RecommendedVideos.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import './RecommendedVideos.scss'
+import VideoCard from '../VideoCard/VideoCard';
 
 function RecommendedVideos() {
   return (
     <div className="recommend">
-      <h1>videos</h1>
+      <h2>Recommended</h2>
+      <div className="recommended__videos">
+        <VideoCard />
+        <VideoCard />
+        <VideoCard />
+        <VideoCard />
+        <VideoCard />
+        <VideoCard />
+        <VideoCard />
+        <VideoCard />
+
+      </div>
     </div>
   )
 }
---
+++ youtube-clone/src/components/VideoCard/VideoCard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './VideoCard.scss';
+
+function VideoCard() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default VideoCard
