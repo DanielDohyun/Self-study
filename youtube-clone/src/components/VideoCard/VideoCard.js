00afc1cb84c098784df360286f062719ab7af24b
DanielDohyun <kkdh1109@gmail.com>
2020-10-21 21:22:23 -0400

    videoCard setup

--- youtube-clone/src/components/VideoCard/VideoCard.js
+++ youtube-clone/src/components/VideoCard/VideoCard.js
@@ -1,9 +1,21 @@
+import { Avatar } from '@material-ui/core';
 import React from 'react'
 import './VideoCard.scss';
 
-function VideoCard() {
+function VideoCard ({ image, title, channel, views, timestamp, channelImg }) {
     return (
-        <div>
+        <div className="videoCard"> 
+            <img className="videoCard__thumbnail" src={image} alt="" />
+            <div className="videoCard__info" >
+                <Avatar className="videoCard__avatar" alt={channel} src={channelImg} />  
+                <div className="videoCard__text">
+                    <h4>{title}</h4>
+                    <p>{channel}</p>
+                    <p>
+                        {views} * {timestamp}
+                    </p>
+                </div>
+            </div>
             
         </div>
     )
