2330eaf08de54c784a17d679c95570c1a1c3df1d
DanielDohyun <kkdh1109@gmail.com>
2020-10-23 13:58:13 -0400

    style videoRow comp

--- youtube-clone/src/components/VideoRow/VideoRow.js
+++ youtube-clone/src/components/VideoRow/VideoRow.js
@@ -7,10 +7,14 @@ function VideoRow( { views, subs, description, timestamp, channel, title, image 
             <img src={image} alt="" />
             <div className="videoRow__text" >
                 <h3>{title}</h3>
-                <p>
-                    {channel} * {subs} Subscribers {views} views * {timestamp}
+                <p className="videoRow__headline">
+                    {channel} * {" "}
+                    <span className="videoRow__subs">
+                        <span className="videoRow__subsNumber">{subs}</span>  Subscribers
+                    </span> {" "}
+                     {views} views * {timestamp}
                 </p>
-                <p>{description}</p>
+                <p className="videoRow__description">{description}</p>
             </div>
         </div>
     )
