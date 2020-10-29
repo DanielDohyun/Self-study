926498f4a1553f232d426409703cf5205f1d5ebd
DanielDohyun <kkdh1109@gmail.com>
2020-10-29 15:12:34 -0400

    serverside validation

--- twitter-clone/server/index.js
+++ twitter-clone/server/index.js
@@ -13,8 +13,24 @@ app.get('/', (req, res) => {
     });
 })
 
+function isValidMew(mew) {
+    return mew.name && mew.name.toString().trim() !== '' &&
+        mew.content && mew.content.toString().trim() !== '';
+}
+
 app.post('/mews', (req, res) => {
-    console.log(req.body);
+    // console.log(req.body);
+    if(isValidMew(req.body)) {
+        // insert into db
+        const mew = {
+            name: req.body.name.toString(),
+            content: req.body.content.toString()
+        }
+        console.log(mew);
+    } else res.status(422);
+        res.json(({
+        message: 'Name and Content are required'
+    }))
 })
 
 app.listen(5000, () => {
