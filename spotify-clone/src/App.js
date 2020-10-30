a75249e7a8fcbc6f4170ef6c812a57d0a139d9bf
DanielDohyun <kkdh1109@gmail.com>
2020-10-30 13:08:38 -0400

    use useState and useEffect to get token from url

--- spotify-clone/src/App.js
+++ spotify-clone/src/App.js
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './App.css';
 import Login from './components/Login/Login';
+import { getTokenFromUrl } from "./spotify";
 
 function App() {
+  const [token, setToken] = useState(null);
+
+
+  // run code based on a given condition
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+
+    // to hide access token from url
+    window.location.hash= "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token)
+    }
+
+    console.log('I have a token: ', _token)
+
+    // if you want this to run only once, leave [] as blank. if you put a variable, it will run whenever that variable changes ex [name]
+  }, []);
+
   return (
     <div className="app">
-      <Login />
+      {
+        token ? (
+          <h1>I'm logged in</h1>
+        ) : (
+          <Login />
+        )
+      }
     </div>
   );
 }
--- spotify-clone/src/spotify.js
+++ spotify-clone/src/spotify.js
@@ -15,8 +15,24 @@ const scopes = [
     "user-read-recently-played",
     "user-read-playback-state",
     "user-top-read",
-    "user-modify-playback-state"
+    "user-modify-playback-state",
 ];
+
+export const getTokenFromUrl = () => {
+    return window.location.hash
+    .substring(1)
+    .split("&")
+    // initial = initial value of reduce,
+    // item = item that we get every time we loops thr
+
+    .reduce((initial, item) => {
+        // http://localhost:3000/#access_token=BQBPkqDz5txVtyfrr-VpVM_54disHXyhBozwSeOmJpl07l0EGP2xEwtIBXHVd3b9_PB-rAPKmUBaYdqI4hXYz_kBl1ZJXMgyrfNulBthHsAgyFcNy59nvxU0GG00X8vVdhR4vsiw_40gtesSGzeg6GSlYw&token_type=Bearer&expires_in=3600
+        var parts = item.split("=")
+        // parts 0 = access token and 1 = actual key
+        initial[parts[0]] = decodeURIComponent(parts[1]);
+        return initial;
+    }, {});
+}
 
 export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
     
