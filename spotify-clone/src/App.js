760cfb1ccd73e7a83aa81712cb2db803944ad116
DanielDohyun <kkdh1109@gmail.com>
2020-11-03 16:42:19 -0500

    install spotify-web-api

--- spotify-clone/src/App.js
+++ spotify-clone/src/App.js
@@ -2,6 +2,10 @@ import React, { useEffect, useState } from "react";
 import './App.css';
 import Login from './components/Login/Login';
 import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+
+// creating an instance of spotify inside of the app.
+const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = useState(null);
@@ -16,7 +20,16 @@ function App() {
     const _token = hash.access_token;
 
     if (_token) {
-      setToken(_token)
+      setToken(_token);
+
+      // giving access token to spotify api
+      spotify.setAccessToken(_token);
+
+      // getting user's info
+      spotify.getMe().then(user => {
+        console.log(user);
+      })
+
     }
 
     console.log('I have a token: ', _token)
