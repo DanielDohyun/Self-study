d8aabbf82323af955ccf199c44566958eeadd521
DanielDohyun <kkdh1109@gmail.com>
2020-11-11 15:54:23 -0500

    start Sidebar, Body and Footer components

--- spotify-clone/src/components/Player/Player.js
+++ spotify-clone/src/components/Player/Player.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import './Player.scss';
+import Sidebar from '../Sidebar/Sidebar';
+import Body from '../Body/Body';
+import Footer from '../Footer/Footer';
 
-function Player() {
+function Player({ spotify }) {
     return (
-        <div>
-            <h1>Welcome to spotify</h1>
+        <div className="player">
+            {/* <h1>Welcome to spotify</h1> */}
+            <div className="player__body">
+                <Sidebar/>
+                <Body />
+            </div>
+           
+            <Footer />
         </div>
     )
 }
