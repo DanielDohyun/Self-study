5a007caf73f4e24d6502b89a4771cdd22e6b4516
DanielDohyun <kkdh1109@gmail.com>
2020-11-12 19:04:15 -0500

    style sidebar option component

--- spotify-clone/src/components/Sidebar/Sidebar.js
+++ spotify-clone/src/components/Sidebar/Sidebar.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import SidebarOption from '../SidebarOption/SidebarOption';
 import './Sidebar.scss';
+import PlayCircleFilledIcon from '@material-ui/icons/PlayCircleFilled';
+import HomeIcon from '@material-ui/icons/Home';
+import SearchIcon from '@material-ui/icons/Search';
+import LibraryMusicIcon from '@material-ui/icons/LibraryMusic';
 
 function Sidebar() {
     return (
         <div className="sidebar">
             <img className="sidebar__logo" src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="spotify img"/>
-            <SidebarOption title="Home" />
-            <SidebarOption title="Search" />
-            <SidebarOption title="Your Library" />
+            <SidebarOption Icon={HomeIcon} title="Home" />
+            <SidebarOption Icon={SearchIcon} title="Search" />
+            <SidebarOption Icon={LibraryMusicIcon} title="Your Library" />
+            
+            <br />
+            <strong className="sidebar__title">PLAYLISTS</strong>
+            <hr />
+
         </div>
     )
 }
--- spotify-clone/src/components/SidebarOption/SidebarOption.js
+++ spotify-clone/src/components/SidebarOption/SidebarOption.js
@@ -4,7 +4,9 @@ import './SidebarOption.scss';
 function SidebarOption({ title, Icon }) {
     return (
         <div className="sidebarOption">
-            <p>{title}</p>
+            {Icon && <Icon className="sidebarOption__icon"></Icon>}
+            {/* if we have Icon props => render h4. otherwise, render p tag */}
+            {Icon ? <h4>{title}</h4> : <p>{title}</p>}
         </div>
     )
 }
