08ac9b6e6d0cfac9fea2394bad5531b102fc3633
DanielDohyun <kkdh1109@gmail.com>
2020-11-16 14:31:21 -0500

    basic folder clean up and insert header instagram logo

--- instagram-clone/src/App.js
+++ instagram-clone/src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="app">
-   
+      <div className="app__header">
+        <img className="app__logo" src="https://logos-world.net/wp-content/uploads/2020/04/Instagram-Logo.png" alt="insta" />
+      </div>
     </div>
   );
 }
