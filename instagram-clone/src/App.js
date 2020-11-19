c8ba792b59a7a67c716335ea016332ec8ecd5c6c
DanielDohyun <kkdh1109@gmail.com>
2020-11-18 21:15:29 -0500

    email password auth fxn and signup form

--- instagram-clone/src/App.js
+++ instagram-clone/src/App.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import './App.scss';
 import Post from './components/Post/Post';
-import { db } from './firebase';
+import { db, auth } from './firebase';
 import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
-import { Button } from '@material-ui/core';
+import { Button, Input } from '@material-ui/core';
 
 function getModalStyle() {
   const top = 50;
@@ -33,7 +33,41 @@ function App() {
   const [modalStyle] = React.useState(getModalStyle);
 
   const [open, setOpen] = useState(false);
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [posts, setPosts] = useState([]);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(authUser => {
+      if (authUser) {
+        console.log(authUser);
+        setUser(authUser);
+        
+      } else {
+        setUser(null);
+      }
+    })
+
+    return () => {
+      //perform some cleanup before you refire useEffect
+      unsubscribe();
+    }
+  }, [user, username]);
+
+  const signUp = (e) => {
+    e.preventDefault();
+
+    auth
+    .createUserWithEmailAndPassword(email, password)
+    .then(authUser => {
+      return authUser.user.updateProfile({
+        displayName: username
+      })
+    })
+    .catch(e => alert(e.message))
+  };
 
   // useEffect = runs a piece of code based on a spe condi
   useEffect(()=> {
@@ -55,11 +89,43 @@ function App() {
         onClose={() => setOpen(false)}
       >
          <div style={modalStyle} className={classes.paper}>
-         <h2>I am a modal</h2>  
+          <form>
+            <center  className="app__form">
+              <img
+                className="app__headerImg"
+                src="https://logos-world.net/wp-content/uploads/2020/04/Instagram-Logo.png"
+                alt=""
+              />
+
+              <Input
+                placeholder="username"
+                type="text"
+                value={username}
+                onChange={e => setUsername(e.target.value)}
+              />
+
+              <Input
+                placeholder="email"
+                type="text"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+              />
+
+              <Input
+                placeholder="password"
+                type="password"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+              /> 
+
+              <Button onClick={signUp}>Sign Up</Button>
+
+            </center>
+          </form>
          </div>
       </Modal>
 
-      <Button onClick={() => setOpen(true)}>Sign Up</Button>
+      <Button type="submit" onClick={() => setOpen(true)}>Sign Up</Button>
 
       <div className="app__header">
         <img className="app__logo" src="https://logos-world.net/wp-content/uploads/2020/04/Instagram-Logo.png" alt="insta" />
