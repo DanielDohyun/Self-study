2022fdc6f22608d800dda328e1b8477bb4756d87
DanielDohyun <kkdh1109@gmail.com>
2020-11-23 18:52:27 -0500

    add comment form, and getting comments from firestore fxnality

--- instagram-clone/src/App.js
+++ instagram-clone/src/App.js
@@ -194,7 +194,7 @@ function App() {
        <div className="app__poastsLeft">
         {
             posts.map(({ id, post }) => (
-              <Post key={id} username={post?.username} caption={post?.caption} imgUrl={post?.imgUrl} />
+              <Post key={id} postId={id} username={post?.username} caption={post?.caption} imgUrl={post?.imgUrl} />
             ))
         }
        </div>
--- instagram-clone/src/components/Post/Post.js
+++ instagram-clone/src/components/Post/Post.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Post.scss';
 import Avatar from '@material-ui/core/Avatar';
+import { db } from '../../firebase';
 
-function Post({ username, caption, imgUrl }) {
+function Post({ postId, username, caption, imgUrl }) {
+    const [comments, setComments] = useState([]);
+    const [comment, setComment] = useState('');
+
+    useEffect(() => {
+        let unsubscribe;
+        if(postId) {
+            unsubscribe = db
+            .collection("posts")
+            .doc(postId)
+            .collection("comments")
+            .onSnapshot((snapshot) => {
+                setComments(snapshot.docs.map((doc) => doc.data()));
+            });
+        }
+        
+        return() => {
+            unsubscribe();
+        };
+      
+    }, [postId]); 
+
+    const postComment = (e) => {
+
+    }
 
     return (
         <div className="post">
@@ -13,6 +38,24 @@ function Post({ username, caption, imgUrl }) {
             
             <img className="post__img" src={imgUrl} alt="" />
             <h4 className="post__text"><strong>{username}</strong> {caption}</h4>
+
+            <form className="post__commentBox">
+                <input
+                    className="post__input"
+                    type="text"
+                    placeholder="Add a comment..."
+                    value={comment}
+                    onChange={e => setComment(e.target.value)}
+                />
+                <button
+                    className="post__btn"
+                    disabled={!comment}
+                    type="submit"
+                    onClick={postComment}
+                >
+                    Post    
+                </button>    
+            </form>
         </div>
     )
 }
